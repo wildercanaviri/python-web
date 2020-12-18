0865d381a1b05b0733020217cc9a6a25f1fbb935
wildercanaviri <53300815+wildercanaviri@users.noreply.github.com>
2020-12-17 23:50:59 -0400

    Consulta con recuperacion de la imagen

--- Informacion.py
+++ Informacion.py
@@ -2,33 +2,47 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 import json
 
 class Busqueda:
-    pass
+
     def __init__(self, palabra, opcion):
         self.palabra=palabra.replace(" ","_")
         self.opcion=opcion
     
-    def obtenerPalabra(self):
+    def obtenerInformacion(self):
 
         sparql = SPARQLWrapper("http://dbpedia.org/sparql")
         sparql.setQuery("""
-            PREFIX res: <http://dbpedia.org/resource/>
+        
+        PREFIX res: <http://dbpedia.org/resource/>
         PREFIX dbpedia: <http://dbpedia.org/ontology/>
-        Select ?descripcion where {
+        Select ?descripcion ?imagen where {
 
         res:"""+self.palabra+""" dbpedia:abstract ?descripcion.
+        res:"""+self.palabra+""" dbpedia:thumbnail ?imagen.
+        
         FILTER (langMatches(lang(?descripcion),'"""+self.opcion+"""'))
         }
 
         """)
+
+      
         sparql.setReturnFormat(JSON)
         results = sparql.query().convert()
 
 
-        decripcion_cantante=""
-
+        descripcion_cantante=""
+        direccion_imagen=""
         for result in results["results"]["bindings"]:
-            decripcion_cantante=result["descripcion"]["value"]
-        return decripcion_cantante   
+            descripcion_cantante=result["descripcion"]["value"]
+            direccion_imagen=result["imagen"]["value"]
+
+            
+
+        if(not descripcion_cantante) :
+                descripcion_cantante = "El cantante no tiene biografía, en el idioma seleccionado"
+        
+        arreglo=[descripcion_cantante,direccion_imagen]
+
+        return arreglo
 
     
 
--- index.py
+++ index.py
@@ -10,9 +10,9 @@ def home():
         palabra=request.form['palabra']
         opcion=request.form['opcion'] 
         aux=Busqueda(palabra,opcion)
-        resultado=aux.obtenerPalabra()
-        return render_template('home.html',contenido=resultado,texto=palabra)
+        resultado=aux.obtenerInformacion()
+        return render_template('home.html',contenido=resultado[0],texto=palabra,opcion_marcada=opcion, imagen=resultado[1])
     else:
-        return render_template('home.html',contenido="",texto="")
+        return render_template('home.html',contenido="",texto="", opcion_marcada="es",imagen="https://icon-library.com/images/default-profile-icon/default-profile-icon-17.jpg")
 if __name__ == '__main__':
     app.run(debug=True)
